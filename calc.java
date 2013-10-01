c0ad8e30060a8c972c53921fb1d9c21c37982a5a
Jingyi Guo <jg3421@columbia.edu>
2013-09-30 19:34:32 -0500

    Test

---
+++ calc.java
@@ -0,0 +1,23 @@
+
+public class calc
+{
+   public static void main(String[] args)
+   {for (int row = 0; row < 5; row ++)
+{
+   for (int col = 0; col < 5; col ++)
+   {
+      if (row == col)
+         break;
+      else
+         System.out.print(0);
+   }
+   System.out.println();
+}
+
+
+
+
+
+}
+}
+
